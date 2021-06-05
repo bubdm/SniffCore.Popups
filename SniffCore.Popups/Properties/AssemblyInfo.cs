091f2ee25afe7640ec7eb769d920c9edde562f9c
David Wendland <the.devicenator@gmail.com>
2021-06-05 09:04:28 +0200

    Add XML NS

--- SniffCore.Popups/Properties/AssemblyInfo.cs
+++ SniffCore.Popups/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: ComVisible(false)]
 [assembly: Guid("97768a0b-a82f-485d-a1ca-075045830e91")]
+[assembly: XmlnsDefinition("http://sniffcore.com", "SniffCore.Popups")]
